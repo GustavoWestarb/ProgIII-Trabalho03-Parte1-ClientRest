7192a9189a95045514ca3197b918d897af038da7
Gustavo Westarb <gustavowestarb45@gmail.com>
2019-05-08 23:28:16 -0300

    Implementações das chamadas em ajax para a restapi

--- ClientRest/src/components/MainRest.vue
+++ ClientRest/src/components/MainRest.vue
@@ -1,78 +1,97 @@
 <template>
   <div class="page-container">
-    <form novalidate class="md-layout" @submit.prevent="validateUser">
-      <md-card class="md-layout-item md-size-50 md-small-size-100">
-        <md-card-header>
-          <div class="md-title">Users</div>
-        </md-card-header>
+    <md-app md-waterfall md-mode="overlap">
+      <md-app-toolbar class="md-primary">
+        <span class="md-title">Trabalho 3 - Gustavo Westarb</span>
+      </md-app-toolbar>
 
-        <md-card-content>
-          <div class="md-layout md-gutter">
-            <div class="md-layout-item md-small-size-100">
-              <md-field :class="getValidationClass('firstName')">
-                <label for="first-name">First Name</label>
-                <md-input name="first-name" id="first-name" autocomplete="given-name" v-model="form.firstName" :disabled="sending" />
-                <span class="md-error" v-if="!$v.form.firstName.required">The first name is required</span>
-                <span class="md-error" v-else-if="!$v.form.firstName.minlength">Invalid first name</span>
-              </md-field>
-            </div>
+      <md-app-content>
+        <div class="md-layout">
+          <md-card class="md-layout-item md-size-25 md-small-size-100" style="max-height: 344px">
+            <form novalidate class="md-layout" @submit.prevent="callAjax">
+              <md-card-header>
+                <div class="md-title">Call Rest</div>
+              </md-card-header>
 
-            <div class="md-layout-item md-small-size-100">
-              <md-field :class="getValidationClass('lastName')">
-                <label for="last-name">Last Name</label>
-                <md-input name="last-name" id="last-name" autocomplete="family-name" v-model="form.lastName" :disabled="sending" />
-                <span class="md-error" v-if="!$v.form.lastName.required">The last name is required</span>
-                <span class="md-error" v-else-if="!$v.form.lastName.minlength">Invalid last name</span>
-              </md-field>
-            </div>
-          </div>
+            <md-card-content>
+              <div class="md-layout md-gutter">
+                <div class="md-layout-item md-small-size-100" >
+                  <md-radio v-model="radio" value="get" v-on:change="onChangeRadio">Get</md-radio>
+                  <md-radio v-model="radio" value="post" v-on:change="onChangeRadio">Post</md-radio>
+                  <md-radio v-model="radio" value="put" v-on:change="onChangeRadio">Put</md-radio>
+                  <md-radio v-model="radio" value="delete" v-on:change="onChangeRadio">Delete</md-radio>
+                </div>
+              </div>
 
-          <div class="md-layout md-gutter">
-            <div class="md-layout-item md-small-size-100">
-              <md-field :class="getValidationClass('gender')">
-                <label for="gender">Gender</label>
-                <md-select name="gender" id="gender" v-model="form.gender" md-dense :disabled="sending">
-                  <md-option></md-option>
-                  <md-option value="M">M</md-option>
-                  <md-option value="F">F</md-option>
-                </md-select>
-                <span class="md-error">The gender is required</span>
-              </md-field>
-            </div>
+              <div class="md-layout md-gutter">
+                <div class="md-layout-item md-small-size-100">
+                  <md-field :class="getValidationClass('gender')">
+                    <label for="identifier">Indentificador</label>
+                    <md-input type="number" id="identifier" name="identifier" v-model="form.id" :disabled="disableId"/>
+                  </md-field>
+                </div>
 
-            <div class="md-layout-item md-small-size-100">
-              <md-field :class="getValidationClass('age')">
-                <label for="age">Age</label>
-                <md-input type="number" id="age" name="age" autocomplete="age" v-model="form.age" :disabled="sending" />
-                <span class="md-error" v-if="!$v.form.age.required">The age is required</span>
-                <span class="md-error" v-else-if="!$v.form.age.maxlength">Invalid age</span>
-              </md-field>
-            </div>
-          </div>
+                <div class="md-layout-item md-small-size-100">
+                  <md-field :class="getValidationClass('age')">
+                    <label for="name">Nome</label>
+                    <md-input type="text" id="name" name="name" v-model="form.name" :disabled="disableName"/>
+                  </md-field>
+                </div>
+              </div>
 
-          <md-field :class="getValidationClass('email')">
-            <label for="email">Email</label>
-            <md-input type="email" name="email" id="email" autocomplete="email" v-model="form.email" :disabled="sending" />
-            <span class="md-error" v-if="!$v.form.email.required">The email is required</span>
-            <span class="md-error" v-else-if="!$v.form.email.email">Invalid email</span>
-          </md-field>
-        </md-card-content>
+              <div class="md-layout md-gutter">
+                <div class="md-layout-item md-small-size-100">
+                  <md-field :class="getValidationClass('gender')">
+                    <label for="age">Idade</label>
+                    <md-input type="number" id="age" name="age" autocomplete="age" v-model="form.age" :disabled="disableAge"/>
+                  </md-field>
+                </div>
 
-        <md-progress-bar md-mode="indeterminate" v-if="sending" />
+                <div class="md-layout-item md-small-size-100">
+                  <md-field :class="getValidationClass('age')">
+                    <label for="salary">Salário</label>
+                    <md-input type="number" id="salary" name="salary" v-model="form.salary" :disabled="disableSalary"/>
+                  </md-field>
+                </div>
+              </div>
+            </md-card-content>
 
-        <md-card-actions>
-          <md-button type="submit" class="md-primary" :disabled="sending">Create user</md-button>
-        </md-card-actions>
-      </md-card>
+            <md-progress-bar md-mode="indeterminate"/>
 
-      <md-snackbar :md-active.sync="userSaved">The user {{ lastUser }} was saved with success!</md-snackbar>
-    </form>
+            <md-card-actions>
+              <md-button type="submit" class="md-primary" :disabled="sending">Send Request</md-button>
+            </md-card-actions>
+          </form>
+          
+          <md-snackbar :md-active.sync="userSaved">The user {{ lastUser }} was saved with success!</md-snackbar>
+          </md-card>
+
+          <md-table v-model="employeesData" md-card class="md-layout-item md-size-70 md-small-size-100" ref="table">
+            <md-table-toolbar>
+              <h1 class="md-title">Employees</h1>
+            </md-table-toolbar>
+
+            <md-table-row slot="md-table-row" slot-scope="{ item }">
+              <md-table-cell md-label="ID" md-sort-by="id" md-numeric>{{ item.id }}</md-table-cell>
+              <md-table-cell md-label="Nome" md-sort-by="name">{{ item.name }}</md-table-cell>
+              <md-table-cell md-label="Salário" md-sort-by="salary">{{ item.salary }}</md-table-cell>
+              <md-table-cell md-label="Idade" md-sort-by="age">{{ item.age }}</md-table-cell>
+            </md-table-row>
+          </md-table>
+        </div>
+
+    </md-app-content>
+
+  </md-app>
   </div>
 </template>
 
 <script>
 
+let urlRest = "http://dummy.restapiexample.com/api/v1"
+
 import { validationMixin } from 'vuelidate'
+import axios from "axios";
 import {
   required,
   email,
@@ -84,15 +103,20 @@ export default {
   name: 'FormValidation',
   mixins: [validationMixin],
   data: () => ({
+    radio: "get",
     form: {
-      firstName: null,
-      lastName: null,
-      gender: null,
+      name: null,
+      salary: null,
       age: null,
-      email: null,
+      id: null
     },
+    employeesData: [],
     userSaved: false,
     sending: false,
+    disableId: false,
+    disableName: true,
+    disableSalary: true,
+    disableAge: true,
     lastUser: null
   }),
   validations: {
@@ -128,40 +152,133 @@ export default {
         }
       }
     },
-    clearForm () {
-      this.$v.$reset()
-      this.form.firstName = null
-      this.form.lastName = null
-      this.form.age = null
-      this.form.gender = null
-      this.form.email = null
-    },
-    saveUser () {
+    callAjax () {
       this.sending = true
 
-      // Instead of this timeout, here you can call your API
-      window.setTimeout(() => {
-        this.lastUser = `${this.form.firstName} ${this.form.lastName}`
-        this.userSaved = true
-        this.sending = false
-        this.clearForm()
-      }, 1500)
-    },
-    validateUser () {
-      this.$v.$touch()
+      if(this.radio == "get"){
+        this.form.age = null
+        this.form.salary = null
+        this.getEmploye(this)
+      }else if(this.radio == "post"){
+        this.postEmployee(this)
+      }else if(this.radio == "put"){
+        this.putEmployee(this)
+      }
+      else{
 
-      if (!this.$v.$invalid) {
-        this.saveUser()
+      }
+
+      // if (!this.$v.$invalid) {
+      //   this.saveUser()
+      // }
+    },
+    getEmploye(args){
+
+      var urlGet;
+      var withId = args.form.id !== null
+
+      if(withId){
+        urlGet = "http://dummy.restapiexample.com/api/v1/employee/" + args.form.id
+      }
+      else{
+        urlGet = "http://dummy.restapiexample.com/api/v1/employees"
+      }
+
+      axios.get(urlGet)
+      .then(response => {
+
+        if(withId){
+          args.employeesData = [];
+          args.employeesData.push({
+            id: response.data.id,
+            name: response.data.employee_name,
+            salary: response.data.employee_salary,
+            age: response.data.employee_age
+          })
+        }else{
+          args.employeesData = [];
+          for (let i = 0; i < 10; i++) {
+            args.employeesData.push({
+              id: response.data[i].id,
+              name: response.data[i].employee_name,
+              salary: response.data[i].employee_salary,
+              age: response.data[i].employee_age
+            })
+          }
+        }
+      })
+      .catch(error => {
+        console.log(error)
+      })
+
+      args.sending = false
+    },
+    postEmployee(args){
+
+      axios.post("http://dummy.restapiexample.com/api/v1/create",
+        '{"name":"'+ args.form.name +'", "salary":"'+ args.form.salary +'","age":"'+ args.form.age +'"}'
+      ).then(response => {
+        args.employeesData.push({
+          id: response.data.id,
+          name: response.data.name,
+          salary: response.data.salary,
+          age: response.data.age
+        })
+      }).catch(error => {
+        console.log(error)
+      })
+
+      args.sending = false
+    },
+    putEmployee(args){
+      let urlPut = "http://dummy.restapiexample.com/api/v1/update/" + args.form.id 
+
+      let postionEmployee = args.employeesData.findIndex(x => x.id == args.form.id)
+      let name = args.form.name === null ? args.employeesData[postionEmployee].name : args.form.name
+      let age = args.form.age === null ? args.employeesData[postionEmployee].age : args.form.age
+      let salary = args.form.salary === null ? args.employeesData[postionEmployee].salary : args.form.salary
+
+      axios.put(urlPut,
+        '{"name":"'+ name +'", "salary":"'+ age +'","age":"'+ salary +'"}'
+      ).then(response => {
+        args.employeesData[postionEmployee] = JSON.parse('{"id":"' + args.form.id + '","name":"'+ response.data.name + '","salary":"'+ response.data.salary+ '","age":"' + response.data.age+'"}')
+        args.radio = "get"
+        args.radio = "put"
+      }).catch(error => {
+        console.log(error)
+      })
+
+      args.sending = false
+    },
+    deleteEmploye(args){
+      
+    },
+    onChangeRadio(){
+      if (this.radio == "put" || this.radio == "post") {
+        this.disableId = this.radio == "post"
+        this.disableName = false
+        this.disableSalary = false
+        this.disableAge = false
+      }else if( this.radio == "delete"){
+        this.disableName = false
+        this.disableSalary = true
+        this.disableAge = true
+      }
+      else{
+        this.disableId = false
+        this.disableName = true
+        this.disableSalary = true
+        this.disableAge = true
       }
     }
   }
 }
 </script>
+
 <style>
-  .md-progress-bar {
-    position: absolute;
-    top: 0;
-    right: 0;
-    left: 0;
+
+  .md-app {
+    height: 100%;
+    border: 1px solid rgba(#000, .12);
   }
 </style>
